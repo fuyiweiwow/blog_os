79320bec3b5b499dd0e73f64314b0a4840e42e3c
FYW <601556811@qq.com>
2026-02-12 13:33:59 +0800

    Add basic support of testframework

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,91 @@
 #![no_std]  // Disable Rust standard library
 #![no_main] // Disable all Rust-level entry points
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"] // Act like redirect the "test_main" to test_runner
 
 use core::panic::PanicInfo;
 mod vga_buffer;
+mod serial;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success     = 0x10,
+    Failed      = 0x11
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4); // 0xf4 is iobase of isa-debug-exit device
+        port.write(exit_code as u32); // Write to port to tell that we need exit
+    }
+
+}
 
 // Used as the entry point of the OS
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
     
+    // Test entry point
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
 
 // Will be called on panic
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_print!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
+
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T 
+where 
+    T : Fn(), 
+{
+    fn run(&self) -> () {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
+
+
+/*
+    SyntaxTip: 
+        &[&dyn Fn()] is a slice of trait object references of the Fn() trait
+        It is basically a list of references to types that can be called like a function
+*/
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Testable]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test.run();
+    }
+
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1);
 }
