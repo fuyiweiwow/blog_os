84247e8878a20d44c901fe0e39446385e46369b3
FYW <601556811@qq.com>
2026-02-12 11:03:08 +0800

    Add println! macro based on vga_buffer

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 #![no_std]  // Disable Rust standard library
 #![no_main] // Disable all Rust-level entry points
 
-use core::{fmt::Write, panic::PanicInfo};
+use core::panic::PanicInfo;
 mod vga_buffer;
 
 // Used as the entry point of the OS
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-    write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
+    println!("Hello World{}", "!");
     
     loop {}
 }
