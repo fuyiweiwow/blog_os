f9a092a20ab299effffcd667363dffb3ae97858f
FYW <601556811@qq.com>
2026-02-26 15:36:24 +0800

    Add and use a fixed_size_block allocator

--- src/allocator.rs
+++ src/allocator.rs
@@ -15,12 +15,13 @@ use x86_64:: {
 
 pub mod bump;
 pub mod linked_list;
+pub mod fixed_size_block;
 
-use linked_list::LinkedListAllocator;
+use fixed_size_block::FixedSizeBlockAllocator;
 use crate::allocator::bump::Locked;
 
 #[global_allocator]
-static ALLOCATOR: Locked<LinkedListAllocator> = Locked::new(LinkedListAllocator::new());
+static ALLOCATOR: Locked<FixedSizeBlockAllocator> = Locked::new(FixedSizeBlockAllocator::new());
 
 pub const HEAP_START: usize = 0x_4444_4444_0000;
 pub const HEAP_SIZE: usize = 100 * 1024; // 100 KiB
